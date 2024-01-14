65ac78df3767d368d1766d78dcd7756804d45915
Ivo Adriel <ivoadriell@hotmail.com>
2024-01-14 20:11:24 -0300

    First_Merge
    Consolidade Changes

--- Assets/Script/InGameMenuManager.cs
+++ Assets/Script/InGameMenuManager.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class InGameMenuManager : MonoBehaviour
 {
-
-    public UnityEngine.UI.Button backToMenu;
+    public Button backToMenu;
 
     private void Start()
     {
+        backToMenu.gameObject.SetActive(false);
         backToMenu.onClick.AddListener(ExitToMainMenu);
+    }
+
+    private void Update()
+    {
+        InGameMenu();
+    }
+
+    private void InGameMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            backToMenu.gameObject.SetActive(!backToMenu.gameObject.activeSelf);
+        }
     }
 
     public void ExitToMainMenu()
