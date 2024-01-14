ad47f33d6d38c816324daf37ee7ceac8451eebf3
Ivo Adriel <ivoadriell@hotmail.com>
2024-01-14 19:38:55 -0300

    Squashed commit of the following:
    commit d3a9fb477d6cc1d3b9b938606d69580a2441f71b
    Author: Ivo Adriel <ivoadriell@hotmail.com>
    Date:   Sun Jan 14 19:26:24 2024 -0300
    
        Added_models
    
        Models from buildings added
    
    commit a2cbd3026d91516c194b8b1dfcb94980cf400039
    Author: Ivo Adriel <ivoadriell@hotmail.com>
    Date:   Sun Jan 14 17:29:04 2024 -0300
    
        Camera Movement
    
        Added camera movement functionality.

---
+++ Assets/Script/CameraManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class CameraManager : MonoBehaviour
+{
+
+    float mDelta = 20; // Pixels. The width border at the edge in which the movement work
+
+    [SerializeField] private float mSpeed = 20.0f; // Scale. Speed of the movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if on the right edge and move accordingly
+        if (Input.mousePosition.x >= Screen.width - mDelta)
+        {
+            transform.position += (Vector3.right + Vector3.back) * Time.deltaTime * mSpeed;
+        }
+        if (Input.mousePosition.x <= 0 + mDelta)
+        {
+            transform.position += (Vector3.left + Vector3.forward) * Time.deltaTime * mSpeed;
+        }
+        if (Input.mousePosition.y >= Screen.height - mDelta)
+        {
+            transform.position += (Vector3.forward + Vector3.right) * Time.deltaTime * mSpeed;
+        }
+        if (Input.mousePosition.y <= 0 + mDelta)
+        {
+            transform.position += (Vector3.back + Vector3.left) * Time.deltaTime * mSpeed;
+        }
+    }
+
+
+}
